62ae4a896d7692173f6c678d789b3e502006d8a2
Duane Northcutt <duane.northcutt@gmail.com>
2023-08-24 14:41:47 -0700

    Stub.

--- components/Si1151/Si1151.h
+++ components/Si1151/Si1151.h
@@ -1,34 +1,156 @@
 #pragma once
 
-#include "esphome.h"
-#include <Si115X.h>
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/i2c/i2c.h"
 
 
-class Si1151 : public PollingComponent, public Sensor {
+namespace esphome {
+namespace Si1151 {
+
+enum Si1151UnitAddress {
+  DEVICE_ADDRESS = 0x53
+};
+
+enum CommandCodes {
+  RESET_CMD_CTR = 0x00,
+  RESET_SW      = 0x01,
+  FORCE         = 0x11,
+  PAUSE         = 0x12,
+  START         = 0x13
+};
+
+enum RegisterAddress {
+  PART_ID    = 0x00,
+  REV_ID     = 0x01,
+  MFR_ID     = 0x02,
+  INFO_0     = 0x03,
+  INFO_1     = 0x04,
+  HOSTIN_3   = 0x07,
+  HOSTIN_2   = 0x08,
+
+  HOSTIN_0   = 0x0A,
+  COMMAND    = 0x0B,
+  IRQ_ENABLE = 0x0F,
+  RESPONSE_0 = 0x11,
+  RESPONSE_1 = 0x10,
+
+  IRQ_STATUS = 0x12,
+  HOSTOUT_0  = 0x13,
+  HOSTOUT_1  = 0x14,
+  HOSTOUT_2  = 0x15,
+  HOSTOUT_3  = 0x16,
+  HOSTOUT_4  = 0x17,
+  HOSTOUT_5  = 0x18,
+  HOSTOUT_6  = 0x19,
+  HOSTOUT_7  = 0x1A,
+  HOSTOUT_8  = 0x1B,
+  HOSTOUT_9  = 0x1C,
+  HOSTOUT_10 = 0x1D,
+  HOSTOUT_11 = 0x1E,
+  HOSTOUT_12 = 0x1F,
+  HOSTOUT_13 = 0x20,
+  HOSTOUT_14 = 0x21,
+  HOSTOUT_15 = 0x22,
+  HOSTOUT_16 = 0x23,
+  HOSTOUT_17 = 0x24,
+  HOSTOUT_18 = 0x25,
+  HOSTOUT_19 = 0x26,
+  HOSTOUT_20 = 0x27,
+  HOSTOUT_21 = 0x28,
+  HOSTOUT_22 = 0x29,
+  HOSTOUT_23 = 0x2A,
+  HOSTOUT_24 = 0x2B,
+  HOSTOUT_25 = 0x2C
+};
+
+enum ParameterAddress {
+  I2C_ADDR     = 0x00,
+  CHAN_LIST    = 0x01,
+
+  ADCCONFIG_0  = 0x02,
+  ADCSENS_0    = 0x03,
+  ADCPOST_0    = 0x04,
+  MEASCONFIG_0 = 0x05,
+
+  ADCCONFIG_1  = 0x06,
+  ADCPOST_1    = 0x08,
+  ADCSENS_1    = 0x07,
+  MEASCONFIG_1 = 0x09,
+
+  ADCCONFIG_2  = 0x0A,
+  ADCSENS_2    = 0x0B,
+  ADCPOST_2    = 0x0C,
+  MEASCONFIG_2 = 0x0D,
+
+  ADCCONFIG_3  = 0x0E,
+  ADCSENS_3    = 0x0F,
+  ADCPOST_3    = 0x10,
+  MEASCONFIG_3 = 0x11,
+
+  ADCCONFIG_4  = 0x12,
+  ADCSENS_4    = 0x13,
+  ADCPOST_4    = 0x14,
+  MEASCONFIG_4 = 0x15,
+
+  ADCCONFIG_5  = 0x16,
+  ADCSENS_5    = 0x17,
+  ADCPOST_5    = 0x18,
+  MEASCONFIG_5 = 0x19,
+
+  MEASRATE_H   = 0x1A,
+  MEASRATE_L   = 0x1B,
+  MEASCOUNT_0  = 0x1C,
+  MEASCOUNT_1  = 0x1D,
+  MEASCOUNT_2  = 0x1E,
+
+  LED1_A       = 0x1F,
+  LED1_B       = 0x20,
+  LED2_A       = 0x21,
+  LED2_B       = 0x22,
+  LED3_A       = 0x23,
+  LED3_B       = 0x24,
+
+  THRESHOLD0_H = 0x25,
+  THRESHOLD0_L = 0x26,
+  THRESHOLD1_H = 0x27,
+  THRESHOLD1_L = 0x28,
+  THRESHOLD2_H = 0x29,
+  THRESHOLD2_L = 0x2A,
+
+  BURST        = 0x2B
+};
+
+
+class Si1151Component : public PollingComponent, public i2c::I2CDevice {
   public:
-    Si115X sls;
-    Sensor *ir_sensor = new Sensor();
-    Sensor *vis_sensor = new Sensor();
-    Sensor *uv_sensor = new Sensor();
+    void set_ir_sensor(sensor::Sensor ir_sensor) { ir_sensor_ = ir_sensor; }
+    void set_visible_sensor(sensor::Sensor visible_sensor) { visible_sensor_ = visible_sensor; }
+    void set_uv_sensor(sensor::Sensor uv_sensor) { uv_sensor_ = uv_sensor; }
 
-    Si1151() : PollingComponent(60000) {};
-
+    void setup() override;
     float get_setup_priority() const override {
       return setup_priority::DATA;
     };
+    void dump_config() override;
+    void update() override;
 
-    void setup() override { sls.Begin(); };
+  protected:
+  i2c::ErrorCode read_data_register_(uint8_t a_register, uint16_t &data) {
+    uint8_t buffer[2];
+    auto retval = this->read_register(a_register, buffer, 2);
+    if (retval == i2c::ERROR_OK)
+      data = (uint16_t(buffer[1]) << 8) | (uint16_t(buffer[0]) & 0xFF);
+    return retval;
+  }
+  i2c::ErrorCode write_config_register_(uint8_t a_register, uint8_t data) {
+    return this->write_register(a_register, &data, 1);
+  }
 
-    void update() override {
-      uint16_t ir = sls.ReadHalfWord();
-      ir_sensor->publish_state(ir);
-      uint16_t vis = sls.ReadHalfWord_VISIBLE();
-      vis_sensor->publish_state(vis);
-      float uv = sls.ReadHalfWord_UV();
-      uv_sensor->publish_state(uv);
-    };
-    /*
-    void setup() override {};
-    void update() override { publish_state(42.0); };
-    */
+  sensor::Sensor *ir_sensor_{nullptr};
+  sensor::Sensor *visible_sensor_{nullptr};
+  sensor::Sensor *uv_sensor_{nullptr};
 };
+
+}  // namespace Si1151
+}  // namespace esphome
