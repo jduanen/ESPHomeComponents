e769a7428000de87ea562eb9a4147e20ebbc109d
Duane Northcutt <duane.northcutt@gmail.com>
2023-08-29 10:25:27 -0700

    Checkpoint.

--- components/Si1151/Si1151.cpp
+++ components/Si1151/Si1151.cpp
@@ -1,3 +1,6 @@
+// Datasheet: https://www.silabs.com/documents/public/data-sheets/si115x-datasheet.pdf
+// User's Guide: https://www.silabs.com/documents/public/application-notes/AN950-Si1153-UG.pdf
+
 #include "Si1151.h"
 #include "esphome/core/log.h"
 #include "esphome/core/hal.h"
@@ -8,9 +11,29 @@ namespace Si1151 {
 
 static const char *const TAG = "Si1151";
 
+
+void Si1151Component::set_ir_sensor(sensor::Sensor *ir_sensor) {
+  ir_sensor_ = ir_sensor;
+  enabled_channels_.set(IR_CHANNEL);
+};
+
+void Si1151Component::set_visible_sensor(sensor::Sensor *visible_sensor) {
+  visible_sensor_ = visible_sensor;
+  enabled_channels_.set(VISIBLE_CHANNEL);
+};
+
+void Si1151Component::set_uv_sensor(sensor::Sensor *uv_sensor) {
+  uv_sensor_ = uv_sensor;
+  enabled_channels_.set(UV_CHANNEL);
+};
+
 void Si1151Component::setup() {
   uint8_t id;
   ESP_LOGCONFIG(TAG, "Setting up Si1151... 3");
+
+  num_channels_ = ((enabled_channels_.test(IR_CHANNEL) ? 1 : 0) +
+	 	   (enabled_channels_.test(VISIBLE_CHANNEL) ? 1 : 0) +
+                   (enabled_channels_.test(UV_CHANNEL) ? 1 : 0));
 
   if (this->read_register(PART_ID, &id, 1) != i2c::ERROR_OK) {
     this->mark_failed();
@@ -22,69 +45,21 @@ void Si1151Component::setup() {
     ESP_LOGV(TAG, "Si1151 REV_ID: 0x%02x", id);
   }
   if (this->read_register(MFR_ID, &id, 1) == i2c::ERROR_OK) {
-    ESP_LOGV(TAG, "Si1151 MFR_ID: 0x%02x", id);
+    ESP_LOGVV(TAG, "Si1151 MFR_ID: 0x%02x", id);
   }
   if (this->read_register(INFO_0, &id, 1) == i2c::ERROR_OK) {
-    ESP_LOGV(TAG, "Si1151 INFO_0: 0x%02x", id);
+    ESP_LOGVV(TAG, "Si1151 INFO_0: 0x%02x", id);
   }
   if (this->read_register(INFO_1, &id, 1) == i2c::ERROR_OK) {
-    ESP_LOGV(TAG, "Si1151 INFO_1: 0x%02x", id);
+    ESP_LOGVV(TAG, "Si1151 INFO_1: 0x%02x", id);
   }
+  ESP_LOGV(TAG, "Si1151 Enabled Channels: IR=%s, VIS=%s, UV=%s",
+           (enabled_channels_.test(IR_CHANNEL) ? "true" : "false"),
+           (enabled_channels_.test(VISIBLE_CHANNEL) ? "true" : "false"),
+           (enabled_channels_.test(UV_CHANNEL) ? "true" : "false"));
 
-  bool fail = false;
-  do {
-    if (!send_command_(START)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(CHAN_LIST, 0B000010)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(MEASRATE_H, 0)) {
-      fail = true;
-      break;
-    }
-    // 1 for a base period of 800 us
-    if (!set_parameter_(MEASRATE_L, 1)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(MEASCOUNT_0, 5)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(MEASCOUNT_1, 10)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(MEASCOUNT_2, 10)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(THRESHOLD0_L, 200)) {
-      fail = true;
-      break;
-    }
-    if (!set_parameter_(THRESHOLD0_H, 0)) {
-      fail = true;
-      break;
-    }
-  } while (false);
-  if (fail) {
-    this->mark_failed();
-    ESP_LOGE(TAG, "Si1151 failed setup");
-    return;
-  }
-
+  reset_();
   ESP_LOGV(TAG, "Si1151 successfully set up");
-
-/*
-    Wire.beginTransmission(Si115X::DEVICE_ADDRESS);
-    Wire.write(Si115X::IRQ_ENABLE);
-    Wire.write(0B000010);
-    Wire.endTransmission();
-*/
 };
 
 void Si1151Component::dump_config() {
@@ -102,69 +77,284 @@ void Si1151Component::dump_config() {
 
 void Si1151Component::update() {
   ESP_LOGE(TAG, "update():");
-  if (ir_sensor_ != nullptr) {
+  if (enabled_channels_.test(IR_CHANNEL)) {
     uint16_t ir = read_IR_();
     ESP_LOGV(TAG, "IR: 0x%02x", ir);
     ir_sensor_->publish_state(ir);
   }
-  if (visible_sensor_ != nullptr) {
+  if (enabled_channels_.test(VISIBLE_CHANNEL)) {
     uint16_t vis = read_visible_();
     ESP_LOGV(TAG, "VIS: 0x%02x", vis);
     visible_sensor_->publish_state(vis);
   }
-  if (uv_sensor_ != nullptr) {
-    float uv = read_UV_();
+  if (enabled_channels_.test(UV_CHANNEL)) {
+     float uv = read_UV_();
     ESP_LOGV(TAG, "UV: 0x%02x", uv);
     uv_sensor_->publish_state(uv);
   }
 };
 
-bool Si1151Component::send_command_(uint8_t code) {
-  uint8_t packet[2] = {COMMAND, code};
-  uint8_t resp, cmd_ctr;
+i2c::ErrorCode read_register_(uint8_t reg, uint8_t &data) {
+  // return values:
+  //   0: read successful
+  // !=0: RESPONSE_0 error code
+  uint8_t buffer;
 
-  do {
-    if (this->read_register(RESPONSE_0, &cmd_ctr, 1) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "send_command_: read_register RESPONSE_0 failed");
-      return false;
-    }
-    if (this->write(packet, sizeof(packet)) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "send_command_: write COMMAND data failed");
-      return false;
-    }
-    if (this->read_register(RESPONSE_0, &resp, 1) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "send_command_: read_register RESPONSE_0 failed");
-      return false;
-    }
-  } while (resp > cmd_ctr);
-  return true;
+  i2c::ErrorCode err_code = this->read_register(reg, &buffer, 1);
+  if (err_code & RSP0_CMD_ERR) {
+    ESP_LOGE(TAG, "read_register_() failure: 0x%02x", err_code);
+    return err_code;
+  }
+  data = buffer;
+  return 0;
 };
 
-bool Si1151Component::set_parameter_(uint8_t loc, uint8_t val) {
-  uint8_t pkt_0[2] = {HOSTIN_0, val};
-  uint8_t pkt_1[2] = {COMMAND, (uint8_t)(loc | (0B10 << 6))};
-  uint8_t resp, cmd_ctr;
-
-  do {
-    if (this->read_register(RESPONSE_0, &cmd_ctr, 1) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "set_parameter_: read_register RESPONSE_0 failed");
-      return false;
-    }
-    if (this->write(pkt_0, sizeof(pkt_0)) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "set_parameter_: write HOSTIN_0 failed");
-      return false;
-    }
-    if (this->write(pkt_1, sizeof(pkt_1)) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "set_parameter_: write COMMAND failed");
-      return false;
-    }
-    if (this->read_register(RESPONSE_0, &resp, 1) != i2c::ERROR_OK) {
-      ESP_LOGE(TAG, "set_parameter_: read_register RESPONSE_0 failed");
-      return false;
-    }
-  } while (resp > cmd_ctr);
-  return true;
+i2c::ErrorCode write_register_(uint8_t reg, uint8_t data) {
+  // return values:
+  //   0: read successful
+  // !=0: RESPONSE_0 error code
+  i2c::ErrorCode err_code = this->write_register(reg, &data, 1);
+  if (err_code & RSP0_CMD_ERR) {
+    ESP_LOGE(TAG, "write_register_() failure: 0x%02x", err_code);
+    return err_code;
+  }
+  return 0;
 };
+
+int16_t Si1151Component::wait_until_ready_(void) {
+  // return values:
+  //  <0: error in reading register
+  //   0: device is ready
+  //  >0: RESPONSE_0 error code
+  // 
+  //  RESPONSE0 will go to "RUNNING" after reset and then "SLEEP" after 
+  // initialization is complete.
+  int const MAX_RETRIES = 3;
+  uint8_t response0;
+  uint8_t count = 0;
+  i2c::ErrorCode err_code;
+
+  while (count < MAX_RETRIES) {
+    err_code = read_register_(RESPONSE_0, &response0);
+    if (err_code & RSP0_CMD_ERR) {
+      return err_code;
+    }
+    if (response0 & RSP0_SLEEP) {
+      break;
+    }
+    count++;
+  }
+  return 0;
+};
+
+int16_t Si1151Component::hard_reset_(void) {
+  // return values:
+  //  !0: failure to initialize device
+  //  =0: successful reset  delay(30);  // don't access device less than 25ms after POR
+  if ((int16_t resval = send_command_(RESET_SW))) {
+    return retval;
+  }
+  return reset_();
+};
+
+int16_t Si1151Component::reset_(void) {
+  // return values:
+  //  !0: failure to initialize device
+  //  =0: successful reset
+  uint8_t chans = (int)(enabled_channels_.to_ulong());
+  int16_t resval;
+
+  // wait until device is in SLEEP mode
+  if ((resval = wait_until_ready_())) {
+    return resval;
+  }
+
+  // init global parameters
+  if ((resval = set_parameter_(CHAN_LIST, chans))) {
+    return resval;
+  }
+  ESP_LOGVV(TAG, "Enabled Channels: 0x%02x", chans);
+
+  // only going to use Force mode, so disable everything
+  uint8_t failures = set_parameter_(MEASRATE_H, 0);  // 0 * 800usec -- not used
+  failures += set_parameter_(MEASRATE_L, 0);  // ...
+
+  failures += set_parameter_(MEASCOUNT_0, 0);  // not used
+  failures += set_parameter_(MEASCOUNT_1, 0);  // ...
+  failures += set_parameter_(MEASCOUNT_2, 0);
+
+  failures += set_parameter_(THRESHOLD0_L, 0);  // not used
+  failures += set_parameter_(THRESHOLD0_H, 0);  // ...
+  failures += set_parameter_(THRESHOLD1_L, 0);
+  failures += set_parameter_(THRESHOLD1_H, 0);
+
+  failures += set_parameter_(BURST, 0);  // not used
+
+  failures += set_parameter_(U_THRESHOLD_L, 0);  // not used
+  failures += set_parameter_(U_THRESHOLD_H, 0);  // ...
+  failures += set_parameter_(L_THRESHOLD_L, 0);
+  failures += set_parameter_(L_THRESHOLD_H, 0);
+  if (failures) {
+    ESP_LOGE(TAG, "Failed to initialize");
+    return -1;
+  }
+
+  //// TODO set up the params for the three channels
+  //// FIXME FIXME FIXME
+
+/*
+    retval += Si115xParamSet( si115x_handle, PARAM_CH_LIST, 0x0f);
+
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCCONFIG0, 0x78);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCSENS0, 0x09);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCPOST0, 0x40);
+
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCCONFIG1, 0x4d);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCSENS1, 0x61);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCPOST1, 0x40);
+
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCCONFIG2, 0x41);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCSENS2, 0x61);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCPOST2, 0x50);
+
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCCONFIG3, 0x4d);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCSENS3, 0x07);
+    retval += Si115xParamSet( si115x_handle, PARAM_ADCPOST3, 0x40);
+*/
+  return write_register_(IRQ_ENABLE, &chans);
+};
+
+uint16_t Si1151Component::get_cmd_ctr_(void) {
+  // return results:
+  // !0: failed to get cmd_ctr
+  // =0: successful got cmd_ctr
+  int const MAX_RETRIES = 5;
+  uint8_t count = 0;
+  i2c::ErrorCode err_code;
+  uint8_t cmd_ctr;
+  uint8_t response0;
+
+  if ((err_code = read_register_(RESPONSE_0, &response0))) {
+    return err_code;
+  }
+  cmd_ctr = (response0 & RSP0_CMD_CTR);
+
+  while (count < MAX_RETRIES) {
+    if ((err_code = wait_until_ready_())) {
+      return err_code;
+    }
+    if ((err_code = read_register_(RESPONSE_0, &response0))) {
+      return err_code;
+    }
+    if ((response0 & RSP0_CMD_CTR) == cmd_ctr) {
+      break;
+    } else {
+      cmd_ctr = (response0 & RSP0_CMD_CTR);
+    }
+    count++;
+  }
+  return cmd_ctr;
+};
+
+uint16_t Si1151Component::check_cmd_ctr_(uint8_t cmd_ctr) {
+  // return values:
+  // !0: counter wasn't incremented
+  // =0: cmd_ctr has been incremented
+  int const MAX_RETRIES = 5;
+  uint8_t count = 0;
+  uint8_t response0;
+  i2c::ErrorCode err_code;
+
+  count = 0;
+  while (count < MAX_RETRIES) {
+    if ((err_code = read_register_(RESPONSE_0, &response0))) {
+      return err_code;
+    }
+    //// TODO should I actually test if counter is incremented (modulo), not just different?
+    if ((response0 & RSP0_CMD_CTR) != cmd_ctr) {
+      break;
+    }
+    count++;
+  }
+  return 0;
+};
+
+int16_t Si1151Component::send_command_(uint8_t cmd_code) {
+  // return results:
+  //  !0: failed to send command
+  //  =0: successful command
+  // all commands (except RESET_CMD_CTR and RESET_SW) increment CMD_CTR field in RESPONSE0
+  int const MAX_RETRIES = 5;
+  i2c::ErrorCode err_code;
+  uint8_t response0;
+  uint8_t cmd_ctr;
+  uint16_t retval;
+
+  if ((retval = get_cmd_ctr_())) {
+    return retval;
+  }
+  cmd_ctr = retval;
+
+  uint8_t packet[2] = {COMMAND, cmd_code};
+  if ((err_code = this->write(packet, sizeof(packet)))) {
+    ESP_LOGE(TAG, "send_command_: write COMMAND failed");
+    return err_code;
+  }
+  return check_cmd_ctr(cmd_ctr);
+};
+
+int16_t Si1151Component::read_parameter_(uint8_t addr, uint8_t &param) {
+  // return values:
+  // !0: error in reading parameter
+  // =0: successful read
+  uint8_t buffer;
+  int16_t retval;
+
+  if ((retval = send_command_(PARAM_QUERY_CMD | (addr & RSP1_PARAM_ADDR)))) {
+    return retval;
+  }
+  if ((i2c::ErrorCode err_code = read_register_(RESPONSE_1, &buffer))) {
+    return err_code;
+  }
+  param = buffer;
+  return 0;
+};
+
+int16_t Si1151Component::set_parameter_(uint8_t addr, uint8_t val) {
+  // return values:
+  // !0: failed to set parameter
+  // =0: successfully set parameter
+  uint8_t cmd_ctr;
+  uint8_t response0;
+  uint16_t retval;
+  i2c::ErrorCode err_code;
+
+  if ((retval = get_cmd_ctr_())) {
+    return retval;
+  }
+  cmd_ctr = retval;
+  if ((retval = write_register_(HOSTIN_0, val))) {
+    return retval;
+  }
+  if ((retval = send_command_(PARAM_SET_CMD | (addr & PARAM_ADDR)))) {
+    return retval;
+  }
+  return check_cmd_ctr(cmd_ctr);
+};
+
+int16_t Si1151Component::force() {
+  return send_command_(FORCE_CMD);
+};
+
+//  The data (2 or 3 bytes) for all selected channels is packed into HOSTOUT?
+// registers in order. 
+//  The data for a channel is only considered valid if the channel's associated
+// IRQ bit is set in the IRQ_STATUS register (values should be considered invalid
+// if the bit is clear).
+//  The IRQ_STATUS register is cleared on being read.
+//  Not connecting INT signal. using Mode 1 (i.e., interrupt on each sample), so
+// set THRESH_EN field in ADCPOST? registers to 0.
+// "In an ALS measurement, two measurements are always taken and added together."
 
 int32_t Si1151Component::read_IR_(void) {
   uint8_t data_0, data_1;
--- components/Si1151/Si1151.h
+++ components/Si1151/Si1151.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <bitset>
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
@@ -13,30 +14,34 @@ enum Si1151UnitAddress {
 };
 
 enum CommandCodes {
-  RESET_CMD_CTR = 0x00,
-  RESET_SW      = 0x01,
-  FORCE         = 0x11,
-  PAUSE         = 0x12,
-  START         = 0x13
+  RESET_CTR_CMD   = 0x00,  // aka NOP cmd, CMD_CTR = 0
+  RESET_SW_CMD    = 0x01,  // reset, CMD_CTR = 0xF
+  NEW_ADDR_CMD    = 0x02,  // set new I2C address
+
+  FORCE_CMD       = 0x11,  // start measurements defined in CHAN_LIST, counter index must be 0 in MEASCONFIG?
+  PAUSE_CMD       = 0x12,  // pauses measurements
+  START_CMD       = 0x13,  // aka AUTO cmd, counter index in MEASCONFIG? must not be 0
+
+  PARAM_QUERY_CMD = 0x40,  // read parameter at address (0x40 | PARAM_ADDRESS) into RESPONSE1
+  PARAM_SET_CMD   = 0x80,  // write value in HOSTIN0 to parameter address (0x80 | PARAM_ADDRESS) and into RESPONSE1
 };
 
 enum RegisterAddress {
-  PART_ID    = 0x00,
-  REV_ID     = 0x01,
-  MFR_ID     = 0x02,
+  PART_ID    = 0x00,  // R; Si1151: 0x51, Si1152: 0x52, Si1153: 0x53
+  REV_ID     = 0x01,  // R; 0xMN: M=Major Rev., N=Minor Rev.
+  MFR_ID     = 0x02,  // R; [7:5]=HW Rev., [4:0]=Filter, LED, and Module code
   INFO_0     = 0x03,
   INFO_1     = 0x04,
   HOSTIN_3   = 0x07,
   HOSTIN_2   = 0x08,
-
-  HOSTIN_0   = 0x0A,
-  COMMAND    = 0x0B,
-  IRQ_ENABLE = 0x0F,
-  RESPONSE_0 = 0x11,
-  RESPONSE_1 = 0x10,
-
-  IRQ_STATUS = 0x12,
-  HOSTOUT_0  = 0x13,
+  HOSTIN_1   = 0x09,
+  HOSTIN_0   = 0x0A,  // RW; value to write to param table with PARAM_SET cmd
+  COMMAND    = 0x0B,  // RW; write command codes here
+  IRQ_ENABLE = 0x0F,  // RW; enable interrupts for channels; [5:0]=IE5-0; WR: enable, RD: ready?
+  RESPONSE_1 = 0x10,  // R; readback value from PARAM_QUERY or PARAM_SET
+  RESPONSE_0 = 0x11,  // R; power state and command counter, resets to 0x2F; [7]=RUNNING, [6]=SUSPEND, [5]=SLEEP, [4]=CMD_ERR, [3:0]=CMD_CTR
+  IRQ_STATUS = 0x12,  // CR; interrupt flag status for each channel
+  HOSTOUT_0  = 0x13,  // R; ADC output data, 2 or 3 bytes per selected/active channel, active indicated in IRQ status
   HOSTOUT_1  = 0x14,
   HOSTOUT_2  = 0x15,
   HOSTOUT_3  = 0x16,
@@ -66,67 +71,115 @@ enum RegisterAddress {
 
 enum ParameterAddress {
   I2C_ADDR     = 0x00,
-  CHAN_LIST    = 0x01,
+  CHAN_LIST    = 0x01,  // active channel list
 
-  ADCCONFIG_0  = 0x02,
-  ADCSENS_0    = 0x03,
+  ADCCONFIG_0  = 0x02,  // Channel 0 setup
+  ADCSENS_0    = 0x03,  // ...
   ADCPOST_0    = 0x04,
   MEASCONFIG_0 = 0x05,
 
-  ADCCONFIG_1  = 0x06,
-  ADCPOST_1    = 0x08,
+  ADCCONFIG_1  = 0x06,  // Channel 1 setup
+  ADCPOST_1    = 0x08,  // ...
   ADCSENS_1    = 0x07,
   MEASCONFIG_1 = 0x09,
 
-  ADCCONFIG_2  = 0x0A,
-  ADCSENS_2    = 0x0B,
+  ADCCONFIG_2  = 0x0A,  // Channel 2 setup
+  ADCSENS_2    = 0x0B,  // ...
   ADCPOST_2    = 0x0C,
   MEASCONFIG_2 = 0x0D,
 
-  ADCCONFIG_3  = 0x0E,
-  ADCSENS_3    = 0x0F,
+  ADCCONFIG_3  = 0x0E,  // Channel 3 setup
+  ADCSENS_3    = 0x0F,  // ...
   ADCPOST_3    = 0x10,
   MEASCONFIG_3 = 0x11,
 
-  ADCCONFIG_4  = 0x12,
-  ADCSENS_4    = 0x13,
+  ADCCONFIG_4  = 0x12,  // Channel 4 setup
+  ADCSENS_4    = 0x13,  // ...
   ADCPOST_4    = 0x14,
   MEASCONFIG_4 = 0x15,
 
-  ADCCONFIG_5  = 0x16,
-  ADCSENS_5    = 0x17,
+  ADCCONFIG_5  = 0x16,  // Channel 6 setup
+  ADCSENS_5    = 0x17,  // ...
   ADCPOST_5    = 0x18,
   MEASCONFIG_5 = 0x19,
 
-  MEASRATE_H   = 0x1A,
-  MEASRATE_L   = 0x1B,
-  MEASCOUNT_0  = 0x1C,
-  MEASCOUNT_1  = 0x1D,
+  MEASRATE_H   = 0x1A,  // measure rate
+  MEASRATE_L   = 0x1B,  // ...
+  MEASCOUNT_0  = 0x1C,  // meascount
+  MEASCOUNT_1  = 0x1D,  // ...
   MEASCOUNT_2  = 0x1E,
 
-  LED1_A       = 0x1F,
-  LED1_B       = 0x20,
+  LED1_A       = 0x1F,  // LED current settings
+  LED1_B       = 0x20,  // ...
   LED2_A       = 0x21,
   LED2_B       = 0x22,
   LED3_A       = 0x23,
   LED3_B       = 0x24,
 
-  THRESHOLD0_H = 0x25,
-  THRESHOLD0_L = 0x26,
+  THRESHOLD0_H = 0x25,  // threshold setup
+  THRESHOLD0_L = 0x26,  // ...
   THRESHOLD1_H = 0x27,
   THRESHOLD1_L = 0x28,
-  THRESHOLD2_H = 0x29,
-  THRESHOLD2_L = 0x2A,
 
-  BURST        = 0x2B
+  U_THRSHOLD_H = 0x29,  // upper threshold window setup
+  U_THRSHOLD_L = 0x2A,  // ...
+
+  BURST        = 0x2B,  // burst control
+
+  L_THRSHOLD_H = 0x2C,  // upper threshold window setup
+  L_THRSHOLD_L = 0x2D   // ...
 };
+
+enum Channels {
+  IR_CHANNEL      = 0,
+  VISIBLE_CHANNEL = 1,
+  UV_CHANNEL      = 2
+};
+
+
+uint8_t const MAX_NUM_CHANNELS = 6;
+
+// RESPONSE_0 bit indicies
+uint8_t const RSP0_RUNNING_INDX = 7;
+uint8_t const RSP0_SUSPEND_INDX = 6;
+uint8_t const RSP0_SLEEP_INDX   = 5;
+uint8_t const RSP0_CMD_ERR_INDX = 4;
+
+// RESPONSE_0 CMD_ERR values
+uint8_t const RSP0_INVALID_CMD_ERR  = 0x0;
+uint8_t const RSP0_INVALID_ADDR_ERR = 0x1;
+uint8_t const RSP0_SATURATION_ERR   = 0x2;
+uint8_t const RSP0_BUF_OVERFLOW_ERR = 0x3;
+
+// RESPONSE_0 bit masks
+uint8_t const RSP0_RUNNING = 0x80;  // set after reset
+uint8_t const RSP0_SUSPEND = 0x40;  // set when ADC measurement being made
+uint8_t const RSP0_SLEEP   = 0x20;  // set after initialization done
+uint8_t const RSP0_CMD_ERR = 0x10;  // cleared by RESET_SW_CMD and RESET_CTR_CMD, set on error
+
+// CMD_ERR=0: increments after each successful cmd
+//            reset to 0 on RESET_CTR_CMD command
+//            set to 0xF on RESET_SW_CMD command
+uint8_t const RSP0_CMD_CTR = 0x0F;
+
+// CMD_ERR=1: 0x0=Invalid Command
+//            0x1=Parameter access to invalid location
+//            0x2=Saturation of ADC or overflow of accumulation
+//            0x3=Output buffer overflow (e.g., Burst > 26B)
+uint8_t const RSP0_ERR_CODE = 0x0F
+
+// RESPONSE_1 bit masks
+uint8_t const RSP1_PARAM_ADDR = 0x3F;
+
+// Parameter address mask
+uint8_t const PARAM_ADDR = 0x3F;
 
 
 class Si1151Component : public PollingComponent, public i2c::I2CDevice {
   public:
-    void set_ir_sensor(sensor::Sensor *ir_sensor) { ir_sensor_ = ir_sensor; }
-    void set_visible_sensor(sensor::Sensor *visible_sensor) { visible_sensor_ = visible_sensor; }
-    void set_uv_sensor(sensor::Sensor *uv_sensor) { uv_sensor_ = uv_sensor; }
+    void set_ir_sensor(sensor::Sensor *ir_sensor);
+    void set_visible_sensor(sensor::Sensor *visible_sensor);
+    void set_uv_sensor(sensor::Sensor *uv_sensor);
 
     void setup() override;
     float get_setup_priority() const override {
@@ -136,27 +189,33 @@ class Si1151Component : public PollingComponent, public i2c::I2CDevice {
     void update() override;
 
   protected:
-  i2c::ErrorCode read_data_register_(uint8_t a_register, uint16_t &data) {
-    uint8_t buffer[2];
-    auto retval = this->read_register(a_register, buffer, 2);
-    if (retval == i2c::ERROR_OK)
-      data = (uint16_t(buffer[1]) << 8) | (uint16_t(buffer[0]) & 0xFF);
-    return retval;
-  }
-  i2c::ErrorCode write_config_register_(uint8_t a_register, uint8_t data) {
-    return this->write_register(a_register, &data, 1);
-  }
+    sensor::Sensor *ir_sensor_{nullptr};
+    sensor::Sensor *visible_sensor_{nullptr};
+    sensor::Sensor *uv_sensor_{nullptr};
 
-  sensor::Sensor *ir_sensor_{nullptr};
-  sensor::Sensor *visible_sensor_{nullptr};
-  sensor::Sensor *uv_sensor_{nullptr};
+    i2c::ErrorCode read_register_(uint8_t reg, uint8_t &data);
+    i2c::ErrorCode write_register_(uint8_t reg, uint8_t data);
 
-  bool send_command_(uint8_t code);
-  bool set_parameter_(uint8_t loc, uint8_t val);
+    int16_t Si1151Component::wait_until_ready_(void);
+    int16_t Si1151Component::hard_reset_(void);
+    int16_t Si1151Component::reset_(void);
 
-  int32_t read_IR_(void);
-  int32_t read_visible_(void);
-  float read_UV_(void);
+    uint16_t Si1151Component::get_cmd_ctr_(void);
+    uint16_t Si1151Component::check_cmd_ctr_(uint8_t cmd_ctr);
+
+    int16_t Si1151Component::send_command_(uint8_t cmd_code);
+    int16_t Si1151Component::read_parameter_(uint8_t addr, uint8_t &param);
+    int16_t Si1151Component::set_parameter_(uint8_t addr, uint8_t val);
+
+    int16_t Si1151Component::force();
+
+    int32_t read_IR_(void);
+    int32_t read_visible_(void);
+    float read_UV_(void);
+
+  private:
+    std::bitset<MAX_NUM_CHANNELS> enabled_channels_;
+    uint8_t num_channels_ = 0;
 };
 
 }  // namespace Si1151
