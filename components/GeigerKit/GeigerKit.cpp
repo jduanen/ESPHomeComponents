3f288065c6b75ab5f30a31fb05b32e5ff5d72088
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-26 16:33:39 -0700

    Fresh start.

--- components/GeigerKit/GeigerKit.cpp
+++ components/GeigerKit/GeigerKit.cpp
@@ -1,4 +1,4 @@
-#include "my_component.h"
+#include "GeigerKit.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
 
@@ -7,10 +7,23 @@ namespace geigerkit_sensor_ns {
 
 static const char *TAG = "GeigerKitSensor";
 
-void GeigerKitSensor::setup() {
-  ESP_LOGD(TAG, "Setting up GeigerKitSensor with custom_id: %s", this->custom_id_.c_str());
+GeigerKitSensor::GeigerKitSensor(const std::string &custom_id) {
+  ESP_LOGD(TAG, "Constructor");
+  set_custom_id(custom_id);
 }
 
+void GeigerKitSensor::setup() {
+  ESP_LOGD(TAG, "Setting up GeigerKitSensor with custom_id: %s", this->custom_id_.c_str());
+  //// TODO poke the device and see if it responds as expected
+  if (false) mark_failed();
+}
+
+void GeigerKitSensor::dump_config() { 
+  ESP_LOGCONFIG(TAG, "GeigerKitSensor:");
+  LOG_LOGCONFIG(TAG, "  Custom ID: %s", this->custom_id_.c_str());
+}
+
+/*
 void GeigerKitSensor::loop() {
   uint8_t data;
   while (this->available() > 0) {
@@ -22,13 +35,12 @@ void GeigerKitSensor::loop() {
       this->check_buffer_();
   }
 }
+*/
 
 void GeigerKitSensor::check_buffer_() {
   ESP_LOGW(TAG, "Data read from sensor: %s", format_hex_pretty(this->buffer_).c_str());
   this->buffer_.clear();
 }
-
-void GeigerKitSensor::dump_config() { LOG_SENSOR("", "GeigerKitSensor", this); }
 
 
 }  // namespace geigerkit_sensor_ns
---
+++ components/GeigerKit/__init__.py
@@ -0,0 +1,30 @@
+import esphome.config_validation as cv
+import esphome.codegen as cg
+from esphome.const import CONF_ID
+
+
+CONF_MY_CUSTOM_ID = "GeigerKit_id"
+
+CODEOWNERS = ["@jduanen"]
+DEPENDENCIES = []
+AUTO_LOAD = []
+MULTI_CONF = False
+
+ns = cg.esphome_ns.namespace("geigerkit_sensor_ns")
+GeigerKit = ns.class_("GeigerKit", cg.Component)
+
+CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(GeigerKit),
+        # schema defininition, including options for the component
+    }
+)
+
+# code generation
+async def to_code(config):
+    # declare new component
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    # configure the component
+    #### TODO
