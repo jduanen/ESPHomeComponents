b908653ac44f6840bef199b3b2927152f56ae5f7
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-27 16:14:45 -0700

    Publish data. Clean up.

--- components/geigerkit/geigerkit.cpp
+++ components/geigerkit/geigerkit.cpp
@@ -28,36 +28,20 @@ void GeigerKitSensor::loop() {
       int countsPerMin;
       float uSvPerHr, volts;
       char tmpBuf[32];
-      ESP_LOGD(TAG, "Str: %s", this->buffer_.data());
       memcpy(tmpBuf, this->buffer_.data(), this->buffer_.size());
-      ESP_LOGD(TAG, "Buf: %s", tmpBuf);
       int n = sscanf(tmpBuf, "%d,%f,%f", &countsPerMin, &uSvPerHr, &volts);
       if (n != 3) {
           ESP_LOGE("custom", "Failed to read from GK sensor board: %s", this->buffer_.data());
       }
-      ESP_LOGD(TAG, "cpm: %d, Sv/Hr: %f, volts: %f", countsPerMin, uSvPerHr, volts);
-      /*
       this->sensor->publish_state(countsPerMin);
       this->sensor->publish_state(uSvPerHr);
       this->sensor->publish_state(volts);
-      */
       this->buffer_.clear();
       continue;
     }
     this->buffer_.push_back(data);
   }
 }
-
-/*
-    if (this->buffer_.size() == 4)
-      this->check_buffer_();
-*/
-
-void GeigerKitSensor::check_buffer_() {
-  ESP_LOGD(TAG, "Data read from sensor: %s", format_hex_pretty(this->buffer_).c_str());
-  this->buffer_.clear();
-}
-
 
 }  // namespace geigerkit_ns
 }  // namespace esphome
