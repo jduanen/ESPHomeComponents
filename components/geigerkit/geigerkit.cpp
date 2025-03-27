88987c55dfb21d840e1924efa8c821f1c1e7aafd
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-27 15:52:39 -0700

    Flush UART in setup(). Buffer chars and print str.

--- components/geigerkit/geigerkit.cpp
+++ components/geigerkit/geigerkit.cpp
@@ -10,12 +10,8 @@ static const char *TAG = "GeigerKit";
 void GeigerKitSensor::setup() {
   ESP_LOGD(TAG, "Setting up GeigerKit");
   // Rx-only, no setup needed
-  /*
-  // Clear UART buffer
-  while (this->available())
-    this->read();
-  */
-  if (false) mark_failed();
+  this->flush();
+  ////if (false) mark_failed();
 }
 
 void GeigerKitSensor::dump_config() {
@@ -26,28 +22,24 @@ void GeigerKitSensor::dump_config() {
 void GeigerKitSensor::loop() {
   uint8_t data;
 
-  const uint32_t now = millis();
-  if ((now - this->last_read_) > 500 && this->available()) {
-    ESP_LOGD(TAG, "CHECK");
-    while (this->available())
-      this->read_byte(&data);
-      ESP_LOGD(TAG, "READ: 0x%x", data);
-    this->last_read_ = now;
+  while (this->available() != 0) {
+    this->read_byte(&data);
+    ESP_LOGD(TAG, "Data: 0x%x", data);
+    if (data == '\n') continue;
+    if (data == '\r') {
+      //// TODO publish data
+      ESP_LOGD(TAG, "Str: %s", this->buffer_.data());
+      this->buffer_.clear();
+      continue;
+    }
+    this->buffer_.push_back(data);
   }
+}
 
 /*
-  while (this->available() != 0) {
-    ESP_LOGD(TAG, "DA");
-    this->read_byte(&data);
-    if (this->buffer_.empty() && (data != 0xff))
-      continue;
-    ESP_LOGD(TAG, "Data: 0x%x", data);
-    this->buffer_.push_back(data);
     if (this->buffer_.size() == 4)
       this->check_buffer_();
-  }
 */
-}
 
 void GeigerKitSensor::check_buffer_() {
   ESP_LOGD(TAG, "Data read from sensor: %s", format_hex_pretty(this->buffer_).c_str());
