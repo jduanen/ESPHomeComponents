08cc75fcfc751bb99d5b15ffeb8221521aeebae9
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-27 16:51:45 -0700

    Made sensors public.

--- components/geigerkit/geigerkit.h
+++ components/geigerkit/geigerkit.h
@@ -10,20 +10,20 @@ namespace geigerkit_ns {
 
 class GeigerKitSensor: public Component, public uart::UARTDevice {
   public:
+    sensor::Sensor *counts_per_min_sensor_ = new sensor::Sensor();
+    sensor::Sensor *uSv_per_hr_sensor_ = new sensor::Sensor();
+    sensor::Sensor *volts_sensor_ = new sensor::Sensor();
+
     GeigerKitSensor() = default;
 
     void setup() override;
     void dump_config() override;
     void loop() override;
+
     float get_setup_priority() const { return setup_priority::DATA; };
 
   protected:
-//    uint32_t last_read_ = 0;
     std::vector<uint8_t> buffer_;
-
-  sensor::Sensor *counts_per_min_sensor_ = new sensor::Sensor();
-  sensor::Sensor *uSv_per_hr_sensor_ = new sensor::Sensor();
-  sensor::Sensor *volts_sensor_ = new sensor::Sensor();
 };
 
 }  // namespace geigerkit_ns
