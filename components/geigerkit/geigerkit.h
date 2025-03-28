0c69a507a5cebcde3c2489d81e5f5ded194aea27
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-27 17:17:22 -0700

    Changes for sensors config.

--- components/geigerkit/geigerkit.h
+++ components/geigerkit/geigerkit.h
@@ -10,10 +10,6 @@ namespace geigerkit_ns {
 
 class GeigerKitSensor: public Component, public uart::UARTDevice {
   public:
-    sensor::Sensor *counts_per_min_sensor_ = new sensor::Sensor();
-    sensor::Sensor *uSv_per_hr_sensor_ = new sensor::Sensor();
-    sensor::Sensor *volts_sensor_ = new sensor::Sensor();
-
     GeigerKitSensor() = default;
 
     void setup() override;
@@ -22,8 +18,17 @@ class GeigerKitSensor: public Component, public uart::UARTDevice {
 
     float get_setup_priority() const { return setup_priority::DATA; };
 
+    void set_cpm_sensor(sensor::Sensor *cpm_sensor) { counts_per_min_sensor_ = cpm_sensor; }
+    void set_sieverts_sensor(sensor::Sensor *usv_sensor) { uSv_per_hr_sensor_ = usv_sensor; }
+    void set_volts_sensor(sensor::Sensor *v_sensor) { volts_sensor_ = v_sensor; }
+
+
   protected:
     std::vector<uint8_t> buffer_;
+
+    sensor::Sensor *counts_per_min_sensor_{nullptr};
+    sensor::Sensor *uSv_per_hr_sensor_{nullptr};
+    sensor::Sensor *volts_sensor_{nullptr};
 };
 
 }  // namespace geigerkit_ns
