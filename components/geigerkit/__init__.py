eeaa470412fbabfa2aa3f06c5222b00f43891662
Duane Northcutt <duane.northcutt@gmail.com>
2025-03-27 17:16:37 -0700

    Configured sensor outputs.

--- components/geigerkit/__init__.py
+++ components/geigerkit/__init__.py
@@ -12,6 +12,10 @@ DEPENDENCIES = ["uart"]
 AUTO_LOAD = []
 MULTI_CONF = False
 
+CPM_CONF = "cpm"
+SIEVERTS_CONF = "uSv_hr"
+VOLTS_CONF = "volts"
+
 ns = cg.esphome_ns.namespace("geigerkit_ns")
 GeigerKitSensor = ns.class_(
     "GeigerKitSensor",
@@ -19,23 +23,21 @@ GeigerKitSensor = ns.class_(
     cg.Component
 )
 
-
-
 CONFIG_SCHEMA = cv.All(
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(GeigerKitSensor),
-            cv.Optional("cpm"): sensor.sensor_schema(
+            cv.Optional(CPM_CONF): sensor.sensor_schema(
                 icon=ICON_RADIOACTIVE,
                 accuracy_decimals=1,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
-            cv.Optional("uSv_hr"): sensor.sensor_schema(
+            cv.Optional(SIEVERTS_CONF): sensor.sensor_schema(
                 icon=ICON_RADIOACTIVE,
                 accuracy_decimals=1,
                 state_class=STATE_CLASS_MEASUREMENT,
             ),
-            cv.Optional("volts"): sensor.sensor_schema(
+            cv.Optional(VOLTS_CONF): sensor.sensor_schema(
                 icon=ICON_RADIOACTIVE,
                 accuracy_decimals=1,
                 state_class=STATE_CLASS_MEASUREMENT,
@@ -45,29 +47,6 @@ CONFIG_SCHEMA = cv.All(
     .extend(cv.COMPONENT_SCHEMA)
     .extend(uart.UART_DEVICE_SCHEMA)
 )
-
-'''
-CONFIG_SCHEMA = sensor.sensor_schema(
-    sensor.sensor_schema(
-        GeigerKitSensor,
-        icon=ICON_RADIOACTIVE,
-        accuracy_decimals=1,
-        state_class=STATE_CLASS_MEASUREMENT,
-    )
-    .extend(uart.UART_DEVICE_SCHEMA)
-)
-
-CONFIG_SCHEMA = cv.All(
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(GeigerKitComponent),
-            # schema defininition, including options for the component
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(uart.UART_DEVICE_SCHEMA)
-)
-'''
 
 FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
     "GeigerKitSensor",
@@ -85,5 +64,12 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    # configure the component
-    #### TODO
+    if cpm_config := config.get(CPM_CONF):
+        sens = await sensor.new_sensor(cpm_config)
+        cg.add(var.set_cpm_sensor(sens))
+    if usv_config := config.get(SIEVERTS_CONF):
+        sens = await sensor.new_sensor(usv_config)
+        cg.add(var.set_seiverts_sensor(sens))
+    if v_config := config.get(VOLTS_CONF):
+        sens = await sensor.new_sensor(v_config)
+        cg.add(var.set_volts_sensor(sens))
