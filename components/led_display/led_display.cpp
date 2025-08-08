f8b68a2578cfff42956dbca1f409362a7613ea69
Duane Northcutt <duane.northcutt@gmail.com>
2025-08-07 19:23:07 -0700

    Fixed direction change?

--- components/led_display/led_display.cpp
+++ components/led_display/led_display.cpp
@@ -272,8 +272,8 @@ void LedDisplayComponent::disableRows_() {
 
 void LedDisplayComponent::shiftInPixels_(LedColor_t rowColor, uint rowNum) {
   // clock in and latch all of the given row's pixel data
-//  for (uint col = (this->get_width_internal() - 1); (col >= 0); col--) {
-  for (uint col = 0; (col < this->get_width_internal()); col++) {
+////  for (uint col = 0; (col < this->get_width_internal()); col++) {
+  for (int col = (this->get_width_internal() - 1); (col >= 0); col--) {
       digitalWrite(COL_CLOCK, LOW);
       digitalWrite(COL_DATA, (this->frameBuffer_[rowNum][col] & rowColor));
       digitalWrite(COL_CLOCK, HIGH);
