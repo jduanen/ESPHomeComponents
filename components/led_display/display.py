cecb9618bef7360724f853f9ccca4d3549b72720
Duane Northcutt <duane.northcutt@gmail.com>
2025-08-10 16:33:56 -0700

    Tried to copy MAX7219 for actions.

--- components/led_display/display.py
+++ components/led_display/display.py
@@ -21,14 +21,14 @@ CONF_SCROLL_DELAY = "scroll_delay"
 CONF_SCROLL_DWELL = "scroll_dwell"
 
 
-led_display_ns = cg.esphome_ns.namespace('led_display')
-
-ScrollMode = led_display_ns.enum("ScrollMode")
+integration_ns = cg.esphome_ns.namespace("led_display")
+ScrollMode = integration_ns.enum("ScrollMode")
 SCROLL_MODES = {
     "CONTINUOUS": ScrollMode.CONTINUOUS,
     "STOP": ScrollMode.STOP,
 }
 
+led_display_ns = cg.esphome_ns.namespace('led_display')
 LedDisplayComponent = led_display_ns.class_(
     "LedDisplayComponent", display.DisplayBuffer, cg.PollingComponent
 )
@@ -42,7 +42,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_SCROLL_MODE, default="CONTINUOUS"): cv.enum(
                 SCROLL_MODES, upper=True
             ),
-            cv.Optional(CONF_SCROLL_ENABLE, default=False): cv.boolean,  #### FIXME
+            cv.Optional(CONF_SCROLL_ENABLE, default=True): cv.boolean,
             cv.Optional(
                 CONF_SCROLL_SPEED, default="250ms"
             ): cv.positive_time_period_milliseconds,
@@ -54,7 +54,7 @@ CONFIG_SCHEMA = (
             ): cv.positive_time_period_milliseconds,
         }
     )
-    .extend(cv.polling_component_schema("500ms"))
+    .extend(cv.polling_component_schema("500ms"))   #### TODO think about this
 )
 
 async def to_code(config):
@@ -75,11 +75,7 @@ async def to_code(config):
         cg.add(var.set_writer(lambda_))
 
 
-DisplayVisibilityAction = led_display_ns.class_(
-    "DisplayVisibilityAction", automation.Action
-)
-
-
+# Display Visibility
 LED_DISPLAY_OFF_ACTION_SCHEMA = automation.maybe_simple_id(
     {
         cv.GenerateID(): cv.use_id(LedDisplayComponent),
@@ -94,20 +90,36 @@ LED_DISPLAY_ON_ACTION_SCHEMA = automation.maybe_simple_id(
     }
 )
 
+DisplayVisibilityAction = led_display_ns.class_("DisplayVisibilityAction", automation.Action)
 
 @automation.register_action(
-    "leddisplay.turn_off", DisplayVisibilityAction, LED_DISPLAY_OFF_ACTION_SCHEMA
+    "led_display.turn_off", DisplayVisibilityAction, LED_DISPLAY_OFF_ACTION_SCHEMA
 )
 @automation.register_action(
-    "leddisplay.turn_on", DisplayVisibilityAction, LED_DISPLAY_ON_ACTION_SCHEMA
+    "led_display.turn_on", DisplayVisibilityAction, LED_DISPLAY_ON_ACTION_SCHEMA
 )
-async def leddisplay_visible_to_code(config, action_id, template_arg, args):
+async def led_display_visible_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     cg.add(var.set_state(config[CONF_STATE]))
     return var
 
+# Display Invert
+DisplayInvertAction = led_display_ns.class_("DisplayInvertAction", automation.Action)
 
+@automation.register_action(
+    "led_display.invert_off", DisplayInvertAction, LED_DISPLAY_OFF_ACTION_SCHEMA
+)
+@automation.register_action(
+    "led_display.invert_on", DisplayInvertAction, LED_DISPLAY_ON_ACTION_SCHEMA
+)
+async def led_display_invert_to_code(config, action_id, template_arg, args):
+    var = cg.new_Pvariable(action_id, template_arg)
+    await cg.register_parented(var, config[CONF_ID])
+    cg.add(var.set_state(config[CONF_STATE]))
+    return var
+
+# Display Intensity
 LED_DISPLAY_INTENSITY_SCHEMA = cv.maybe_simple_value(
     {
         cv.GenerateID(): cv.use_id(LedDisplayComponent),
@@ -121,9 +133,9 @@ LED_DISPLAY_INTENSITY_SCHEMA = cv.maybe_simple_value(
 DisplayIntensityAction = led_display_ns.class_("DisplayIntensityAction", automation.Action)
 
 @automation.register_action(
-    "leddisplay.intensity", DisplayIntensityAction, LED_DISPLAY_INTENSITY_SCHEMA
+    "led_display.intensity", DisplayIntensityAction, LED_DISPLAY_INTENSITY_SCHEMA
 )
-async def leddisplay_intensity_to_code(config, action_id, template_arg, args):
+async def led_display_intensity_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     template_ = await cg.templatable(config[CONF_INTENSITY], args, cg.uint8)
