0a706980be1df007ff2856ed62823cd84bbbbc81
Duane Northcutt <duane.northcutt@gmail.com>
2025-08-15 16:26:31 -0700

    Added include for font.

--- components/led_display/led_display.h
+++ components/led_display/led_display.h
@@ -4,6 +4,7 @@
 #include "esphome/core/time.h"
 #include "esphome/components/font/font.h"
 #include "esphome/components/display/display_buffer.h"
+#include "esphome/components/font/font.h"
 
 #include <numeric>  //// TMP TMP TMP
 #include <vector>
