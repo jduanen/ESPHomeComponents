b0c094a52b8715ba8ecd230d9a81eba3c1542ffb
Duane Northcutt <duane.northcutt@gmail.com>
2025-08-19 13:05:08 -0700

    Fixed call to print().

--- components/led_display/led_display.cpp
+++ components/led_display/led_display.cpp
@@ -263,7 +263,6 @@ uint8_t LedDisplayComponent::printLED(uint8_t startPos, const char *str) {
         this->print(xPos, 0, this->currentFont_, LedColorToColor(this->background_),
                     esphome::display::TextAlign::TOP_LEFT, strBuf.c_str(),
                     LedColorToColor(this->currentColor_));
-        //xPos += this->currentFont_->get_string_width(strBuf.c_str());
         xPos += this->getStringWidth_(this->currentFont_, strBuf.c_str());
         ESP_LOGD(TAG, "printLED: %s (%u)", strBuf.c_str(), xPos);
         strBuf.clear();
@@ -276,7 +275,9 @@ uint8_t LedDisplayComponent::printLED(uint8_t startPos, const char *str) {
     // add char to string buffer
     strBuf += str[strIndx++];
   }
-  this->print(xPos, 0, this->currentFont_, this->currentColor_, strBuf.c_str());
+  this->print(xPos, 0, this->currentFont_, LedColorToColor(this->background_),
+              esphome::display::TextAlign::TOP_LEFT, strBuf.c_str(),
+              LedColorToColor(this->currentColor_));
   xPos += this->getStringWidth(this->currentFont_, strBuf.c_str());
   ESP_LOGD(TAG, "final printLED: %s (%u)", strBuf.c_str(), xPos);
   return xPos;
